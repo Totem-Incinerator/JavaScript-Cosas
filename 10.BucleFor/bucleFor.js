c5208d6c7d895dd5c8812a4c888eeabea5cf5b78
Jhonathan Bermudez <jotaceron@hotmail.com>
2021-03-11 10:05:34 -0500

    Bucle for

---
+++ 10.BucleFor/bucleFor.js
@@ -0,0 +1,25 @@
+/*
+	BUCLE FOR
+	For es un bucle determinado ya que hay que 
+	especificar cuantas vueltas dará durante su
+	ejecución.
+
+	sintaxis:
+	for (var i = 0; i < 10; i++) {
+		[i]
+	}
+
+*/
+
+/*
+let numbers = [56,14,23,37,41,59];
+
+for(let i = 0; i<numbers.length; i++){
+	console.log(`i vale ${i} y el valor de esa 
+		posicion en el array es: ${numbers[i]}`);
+}
+*/
+
+
+
+
