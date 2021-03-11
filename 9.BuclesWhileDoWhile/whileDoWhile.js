76de572a6934136afe335f13d76c603d6f060afe
Jhonathan Bermudez <jotaceron@hotmail.com>
2021-03-11 09:59:26 -0500

    Bucles while Do while JS

---
+++ 9.BuclesWhileDoWhile/whileDoWhile.js
@@ -0,0 +1,35 @@
+/*
+	BUCLE WHILE - INDETERMINADO
+	While es un bucle indeterminado ya que no sabemos
+	cuantas vueltas durá durante su ejecución.
+
+	sintaxis
+	while (condition) {
+		// statement
+	}
+
+	BUCLE DO WHILE 
+	Do while es un bucle indeterminado ya que no sabemos
+	cuantas vueltas drá durante su ejecución.
+
+	Sintaxis 
+		Código a ejecutar
+		Condición de salida
+	do{
+		codigo a ejecutar
+	}while(condicion)
+	
+*/
+
+/*
+let pass = '';
+while (pass != 'Hola') {
+	pass = prompt('Introduza su contraseña');
+}
+console.log('Fin del bucle');
+*/
+
+do{
+	pass = prompt('Introduza su contraseña');
+	
+}while (pass != 'hola');
