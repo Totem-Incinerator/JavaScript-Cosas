9a57bd999d24fff689121c60239c3c59f3e99a86
Jhonathan Bermudez <jotaceron@hotmail.com>
2021-03-11 10:08:27 -0500

    Ejercicios JS

---
+++ 12.EjerciciosRepaso/EjerciciosRepaso.js
@@ -0,0 +1,60 @@
+/*
+EJERCICIO 1:
+
+let name = prompt('Ingresa tu nombre');
+let edad = parseInt(prompt('Ingresa tu edad'));
+
+console.log(`Hola ${name}, tienes ${edad} años y el año que viene tendrás ${edad + 1} años!`);
+*/
+
+/*
+
+
+EJERCICIO 2:
+let opcion = parseInt(prompt('Selecciona una opcion para calcular el area' + '\n' +
+					'1. Triangulo' + '\n' +
+					'2. Rectangulo' + '\n' +
+					'3. Circulo'));
+
+switch (opcion) {
+	case 1:
+		let base = parseFloat(prompt('Ingresa la base'));
+		let altura = parseFloat(prompt('Ingresa la altura'));
+		let result = (base * altura) /2;
+		alert(`El area del triangulo es: ${result}`);
+		break;
+	case 2:
+		let base2 = parseFloat(prompt('Ingresa la base'));
+		let altura2 = parseFloat(prompt('Ingresa la altura'));
+		let result2 = base2 * altura2;
+		alert(`El area del rectangulo es: ${result2}`);
+		break;
+	case 3:
+		let radio = parseFloat(prompt('Ingresa el radio'));
+		const PI = 3.14;
+		let result3 = PI * Math.pow(radio,2);
+		alert(`El area del rectangulo es: ${result3}`);
+		break;
+	default:
+		alert('No ha seleccionado una opcion');
+		break;
+}
+*/
+
+
+/*
+EJERCICIO 3:
+let num = parseInt(prompt('Ingresa un numero'));
+
+for (var i = 1; i <= num; i++) {
+
+	if (i % 2 == 0) {
+		console.log(`${i} es par`);
+	} else {
+		console.log(`${i} es impar`);
+	}
+}
+*/
+
+let num = parseInt(prompt('Ingresa un numero'));
+
